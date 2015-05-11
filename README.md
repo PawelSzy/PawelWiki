f2ccd30781154d1a1fc11be7d2997cff8e982546
PawelSz <peal1982@gmail.com>
2015-05-11 21:31:05 +0200

    PawelWiki obsluga bazy danych

---
+++ README.md
@@ -0,0 +1,4 @@
+PawelWiki
+=========
+
+A Symfony project created on May 8, 2015, 9:20 pm.
---
+++ src/PawelWikiBundle/PawelWikiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PawelWikiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PawelWikiBundle extends Bundle
+{
+}
