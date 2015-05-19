b07a351d851b511be74cef9ec614a4f05f4a6d5f
PawelSz <peal1982@gmail.com>
2015-05-19 22:31:48 +0200

    Dodaj define repository_path do StronaControler

--- src/PawelWikiBundle/Controller/WyswietlStroneController.php
+++ src/PawelWikiBundle/Controller/WyswietlStroneController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use PawelWikiBundle\Controller\ArtykulFactory;
 
+define('artykulRepository','PawelWikiBundle:ArtykulDB:ArtykulDB');
+
 //require 'Symfony\PawelWiki\src\PawelWikiBundle\Controller\ArtykulFactory.php';
 
 class WyswietlStroneController extends Controller
@@ -13,14 +15,16 @@ class WyswietlStroneController extends Controller
     public function WyswietlStroneAction($tytul)
     {
 	echo "tytul: ".$tytul."\n";
-
-    $this->repository = $this->getDoctrine()->getRepository('PawelWikiBundle:ArtykulDB:ArtykulDB');
+    //$getRepository = new getActualRepository;
+    $this->repository = $this->getDoctrine()->getRepository(artykulRepository);
+    
     $this->ArtykulFactory = new ArtykulFactory($this->repository);
 
     $artykul = $this->ArtykulFactory->odczytajArtykul($tytul); 
 
 
     var_dump($artykul);
+    echo "<\ br>".$artykul->odczytajTytul()."<\ br>";
     return $this->render('PawelWikiBundle:Default:index.html.twig', array('name' => $tytul));
     }
 }
