531000ae5f49722582834a7ceb1b3a6464ce126d
PawelSz <peal1982@gmail.com>
2015-05-22 22:26:26 +0200

    Dodaj wyswietl artykul do classy wyswietl strone

--- src/PawelWikiBundle/Controller/WyswietlStroneController.php
+++ src/PawelWikiBundle/Controller/WyswietlStroneController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use PawelWikiBundle\Controller\ArtykulFactory;
 
-define('artykulRepository','PawelWikiBundle:ArtykulDB:ArtykulDB');
+//define('artykulRepository','PawelWikiBundle:ArtykulDB:ArtykulDB');
 
 //require 'Symfony\PawelWiki\src\PawelWikiBundle\Controller\ArtykulFactory.php';
 
@@ -14,16 +14,18 @@ class WyswietlStroneController extends Controller
 {
     public function WyswietlStroneAction($tytul)
     {
-	echo "tytul: ".$tytul."\n";
+	// echo "tytul: ".$tytul."\n";
 
-    $this->repository = $this->getDoctrine()->getRepository(artykulRepository);
+ //    const 
+
+ //    $this->repository = $this->getDoctrine()->getRepository(artykulRepository);
     
-    $this->ArtykulFactory = new ArtykulFactory($this->repository);
-    $artykul = $this->ArtykulFactory->odczytajArtykul($tytul); 
+ //    $this->ArtykulFactory = new ArtykulFactory($this->repository);
+ //    $artykul = $this->ArtykulFactory->odczytajArtykul($tytul); 
 
 
-    var_dump($artykul);
-    echo "***********".$artykul->odczytajTytul()."*******************************";
-    return $this->render('PawelWikiBundle:Default:index.html.twig', array('name' => $tytul));
+ //    var_dump($artykul);
+ //    echo "***********".$artykul->odczytajTytul()."*******************************";
+    return $this->render('PawelWikiBundle:Default:wiki_strona.html.twig', array('tytul' => $tytul));
     }
 }
