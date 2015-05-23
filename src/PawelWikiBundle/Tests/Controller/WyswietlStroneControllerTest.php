fea74b9670045e80907957b9f238c2db1c9be40d
PawelSz <peal1982@gmail.com>
2015-05-23 17:32:26 +0200

    Dodaj stopke

--- src/PawelWikiBundle/Tests/Controller/WyswietlStroneControllerTest.php
+++ src/PawelWikiBundle/Tests/Controller/WyswietlStroneControllerTest.php
@@ -10,6 +10,9 @@ class testWyswietlStrone extends WebTestCase
 {
 	public function testWyswietlStrone()
 	{
+		//////////////////////
+		// Test dla przykladowej strony Artykul_test
+		/////////////////////
 		$client = static::createClient();
 		$clawler = $client->request('GET', '/strona/Artykul_test');
 		
@@ -25,5 +28,11 @@ class testWyswietlStrone extends WebTestCase
 		/////////////////////////////////////////////////////////////////////
 		$content = $client->getResponse()->getContent();
 		$this->assertRegExp('/Husaria – polska jazda należąca do autoramentu narodowego, znana z wielu zwycięstw formacja kawaleryjska Rzeczypospolitej, obecna na polach bitew od początku XVI/', $content);		
+	
+		//sprawdz czy istnieje stopka  /////////////////////////////////////////////////////////
+		$this->assertRegExp('/Testowa wersja Wiki stworzona przez Pawla Szymańskiego/', $content);
+		//sprawdzy czy istnieje classa stopka
+		$this->assertRegExp('/class="stopka"/', $content);
+
 	}
 }
