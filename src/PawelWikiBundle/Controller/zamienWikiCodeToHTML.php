fd8561615e591aa7245ea8c7b1befda64872012d
PawelSz <peal1982@gmail.com>
2015-06-04 23:16:51 +0200

    Dodaj zamiane listy z WikiCode na HTML

--- src/PawelWikiBundle/Controller/zamienWikiCodeToHTML.php
+++ src/PawelWikiBundle/Controller/zamienWikiCodeToHTML.php
@@ -7,11 +7,19 @@ class zamienWikiCodeToHTML
 		"/====([^=]*)====/" => '<h4>\1</h4>',
 		"/===([^=]*)===/" => '<h3>\1</h3>' ,
 		"/==([^=]*)==/" => '<h2>\1</h2>',
-		"/''''([^']*)''''/" => '<i><b>\1</b></i>',
-		"/'''([^']*)'''/" => '<b>\1</b>',
-		"/''([^']*)''/" => '<i>\1</i>',		
-		'/(https?):\/\/(([A-Za-z0-9_-]+)\.([A-Za-z0-9_-]+(\/[A-Za-z0-9_-]+)*))/' => '<url="\1://\2">\2</url>',
-		'/^(https?:\/\/)?([\da-z\.-]+).([a-z\.]{2,6})([\/\w \.-]*)*\/?$/' => '<url="\1">\1</url>'
+		"/''''([^\']*)''''/" => '<i><b>\1</b></i>',
+		"/'''([^\']*)'''/" => '<b>\1</b>',
+		"/''([^\']*)''/" => '<i>\1</i>',		
+		'/(https?):\/\/(([A-Za-z0-9_-]+)\.([A-Za-z0-9_-]+((\/|\.)[A-Za-z0-9_-]+)*))/' => '<url="\1://\2">\2</url>',
+		"/\[\[([^\[\]]*)\|([^\[\]]*)\]\]/" => '<url="$router->generate(\'pawel_wiki_artykul\', array(\'tytul\' => \'\1\'))\'">\2</url>',
+		"/\[\[([^\[\]]*)\]\]/" => '$router->generate("pawel_wiki_artykul", array("tytul" => "\1"))',
+		"/\[\[([^\[\]]*)#([^\[\]]*)\]\]/" => '$router->generate("pawel_wiki_artykul", array("tytul" => "\1#\2"))',
+		"/((\*([A-Za-z0-9_-]+))+)+/" => '<li>\3</li>',
+		//"/((<li>.*<\/li>)\s?(<li>.*<\/li>)+)/" => '<ul>\1</ul>',
+		"/((#([A-Za-z0-9_-]+))+)+/" => '<li>\3</li>',
+		"/((<li>.*<\/li>)\s?(<li>.*<\/li>)*)/" => '<ul>\1</ul>',		
+		
+		
 
 	);
 
@@ -30,7 +38,15 @@ $string = "==tekst==   ==tekst2==CCC  ===H3=== ''italic''  '''bold'''  ''''ib'''
 ." https://google.com/"."\n".
 "  https://google.com "."\n".
 "http://go_og_le.com/test"."\n".
-"http://go_og-le.com/test/test2   "."\n".
-"http://google.com/test/test2/test3   ";
+"http://go_og-le.com.pl/test/test2   "."\n".
+"http://google.com/test/test2/test3"."\n".
+"[[husaria]]"."\n".
+"[[husaria#bron]]"."\n".
+"[[husaria|husariiiiii]]"."\n".
+"*tesr"."\n"."*test2"."\n".
+"#tesr3"."\n"."#test4"."\n".
+"tessssssssssssssssss"."\n".
+"*tes5r"."\n"."*test6"."\n"
+;
 echo zamienWikiCodeToHTML::konwersjaWikiCodeToHTML( $string );
 echo "\n";
