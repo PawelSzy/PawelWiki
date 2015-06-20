1b0a32f2ae453049238cc2961265ab3b59329a2a
PawelSz <peal1982@gmail.com>
2015-06-20 16:17:56 +0200

    Dodaj testowanie diff

--- src/PawelWikiBundle/Classes/testDiff.php
+++ src/PawelWikiBundle/Classes/testDiff.php
@@ -14,22 +14,27 @@ $string1 = "
 7
 ";
 
-$string2 = "
+$string2 = "*
 1
 2
 new
 4
+*
+*
 new
 5
 new
 6
 ";
-
+/** 
+Classa zajmuje sie roznica pomiedzy strigami 
+*/
 class StringDiff 
 {
-  const UNMODIFIED = 0;
-  const DELETED    = 1;
-  const INSERTED   = 2;
+
+  // const UNMODIFIED = 0;
+  // const DELETED    = 1;
+  // const INSERTED   = 2;
 
 
 
@@ -52,17 +57,22 @@ class StringDiff
 	// 	return array($diff, $stringDiff);
 	// }
 
-	static function zwrocStarySring($diff, $newString, $znakNowalinia = "\n") 
-	{
-		$i = count($diff);
-		$r = count($newString); 
+  	/**
+  	---- Funkcja zwraca stara wersje tekstu na podstawie nowego i roznicy pomiedzy nimi
+	// @param diff - array zawierajacy roznice pomiedzy dwoma stringami - podzoci z classDiff
+	// @param #newString - nowy string - zostanie skonwertoany do starszej wersji
+	// @param $nowaLinia - znak rozdziajacy odzielne linie stringa
+	// @return - string bedacy starsza wersja
+  	*/
+	static function zwrocStaryString($diff, $newString, $znakNowalinia = "\n") 
+	{ 
 
 		$arrString = explode("\n", $newString);
 		$r = 0;
 		foreach ($diff as $i => $daneLinii)
 		{ 
 			//dodaj stara linie
-			if ($daneLinii[1] ==SELF::DELETED)
+			if ($daneLinii[1] ==Diff::DELETED)
 			{
 				//fnkcja odwrotna do tworzenia
 				//dodaj nowa linie do array
@@ -70,7 +80,7 @@ class StringDiff
 				$r +=1;
 			}
 			//usun linie
-			elseif ($daneLinii[1] ==SELF::INSERTED)
+			elseif ($daneLinii[1] ==DIff::INSERTED)
 			{
 				//fnkcja odwrotna do tworzenia
 				//usun nowa linie do array
@@ -83,7 +93,7 @@ class StringDiff
 			}		
 			echo "i: ".$i."\n";
 			echo "r: ".$r."\n";	
-			echo "**************: "."\n";			
+			echo "**************: \n";			
 		}	
 		return implode("\n", $arrString);
 	}	
@@ -101,10 +111,6 @@ class StringDiff
 	}		
 }
 
-// list($diff, $stringDiff) = StringDiff::pokazRoznice( $string1, $string2 );
-
-// echo "diff: \n";
-// var_dump($diff);
 
 $diff = Diff::compare($string1, $string2 );
 
@@ -119,16 +125,16 @@ echo "\n-------------- \n";
 
 echo Diff::toString($diff);
 
-echo StringDiff::zwrocStarySring($diff, $string2) ;
-//array_splice($arr_alphabet, 2, 0, 'c');
-//unset($array[2]);
-// $array = array_values($array);
+$oldString =  StringDiff::zwrocStaryString($diff, $string2) ;
+echo $oldString;
+echo $string1;
 
 
-// }
+//testowanie klasy
+$ar1 = explode("\n", $string1);
+$ar2 = explode("\n", $oldString);
 
-//$oldString = zwrocStarySring($diff, $newString);
-// echo "Czy udalo sie wrocic do starego tekstu ".(assert($string2 === $string2));  
+echo "Czy udalo sie wrocic do starego tekstu ".assert( strcmp( $oldString, $string1 ) );  
 
 
 //// Uzyj explode aby rozebrac na stringi po linie z podzialem \n 
