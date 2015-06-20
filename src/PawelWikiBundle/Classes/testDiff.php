1627fbf3363c79c2a83c2d110ed05738a42f9b60
PawelSz <peal1982@gmail.com>
2015-06-20 14:40:52 +0200

    Dzialajace zwroc stary string na podstawie differ

--- src/PawelWikiBundle/Classes/testDiff.php
+++ src/PawelWikiBundle/Classes/testDiff.php
@@ -10,14 +10,19 @@ $string1 = "
 3
 4
 5
+6
+7
 ";
 
 $string2 = "
 1
+2
 new
-new
-3
 4
+new
+5
+new
+6
 ";
 
 class StringDiff 
@@ -28,51 +33,58 @@ class StringDiff
 
 
 
-	static function pokazRoznice( $string1, $string2 )
+	// static function pokazRoznice( $string1, $string2 )
+	// {
+
+	// 	$allDiff  = Diff::compare($string1, $string2 );
+	// 	//echo Diff::toString( $allDiff );
+
+	// 	$diff = array();
+	// 	$lenDif = count($allDiff);
+	// 	for ($i=0; $i < $lenDif; $i++) { 
+	// 		if ($allDiff[$i][1] !=SELF::UNMODIFIED)
+	// 		{
+	// 			$diff[(string)$i] = $allDiff[$i]; 
+	// 		}
+	// 	}
+	// 	//var_dump($diff);
+	// 	$stringDiff =  Diff::toString( $diff );
+	// 	return array($diff, $stringDiff);
+	// }
+
+	static function zwrocStarySring($diff, $newString, $znakNowalinia = "\n") 
 	{
+		$i = count($diff);
+		$r = count($newString); 
 
-		$allDiff  = Diff::compare($string1, $string2 );
-		//echo Diff::toString( $allDiff );
-
-		$diff = array();
-		$lenDif = count($allDiff);
-		for ($i=0; $i < $lenDif; $i++) { 
-			if ($allDiff[$i][1] !=SELF::UNMODIFIED)
-			{
-				$diff[(string)$i] = $allDiff[$i]; 
-			}
-		}
-		//var_dump($diff);
-		$stringDiff =  Diff::toString( $diff );
-		return array($diff, $stringDiff);
-	}
-
-	static function zwrocStarySring($diff, $newString) 
-	{
 		$arrString = explode("\n", $newString);
-		echo "*******************************\n";
-		//var_dump($arrString);
-		//$lenDif = count($diff);
-		//krsort($diff); //sortuj od najwiekszego key do najmiejszego
-		foreach ($diff as $nrLinii => $daneLinii)
+		$r = 0;
+		foreach ($diff as $i => $daneLinii)
 		{ 
 			//dodaj stara linie
 			if ($daneLinii[1] ==SELF::DELETED)
 			{
 				//fnkcja odwrotna do tworzenia
 				//dodaj nowa linie do array
-				$arrString = SELF::dodajLinie( $arrString, $nrLinii, $daneLinii[0] );
+				$arrString = SELF::dodajLinie( $arrString, $r, $daneLinii[0] );
+				$r +=1;
 			}
 			//usun linie
-			if ($daneLinii[1] ==SELF::INSERTED)
+			elseif ($daneLinii[1] ==SELF::INSERTED)
 			{
 				//fnkcja odwrotna do tworzenia
 				//usun nowa linie do array
-				$arrString = SELF::usunLinie( $arrString, $nrLinii );
-			}			
+				$arrString = SELF::usunLinie( $arrString, $r );
+ 			
+			}
+			else 
+			{
+				$r +=1;
+			}		
+			echo "i: ".$i."\n";
+			echo "r: ".$r."\n";	
+			echo "**************: "."\n";			
 		}	
-		//var_dump($arrString);
-		echo "\n*******************************\n";
 		return implode("\n", $arrString);
 	}	
 
@@ -83,28 +95,31 @@ class StringDiff
 	}	
 	static function usunLinie( $array, $nrLinii)
 	{
-		echo "nrLinii ".$nrLinii."\n";
-		var_dump($array);
 		unset($array[$nrLinii]);
-		echo "unset\n";
-		echo "nrLinii ".$nrLinii."\n";
-		var_dump($array);
 		$array = array_values($array);
-		echo "array Val\n";
-		var_dump($array);
 		return $array;
 	}		
 }
 
-list($diff, $stringDiff) = StringDiff::pokazRoznice( $string1, $string2 );
+// list($diff, $stringDiff) = StringDiff::pokazRoznice( $string1, $string2 );
+
+// echo "diff: \n";
+// var_dump($diff);
+
+$diff = Diff::compare($string1, $string2 );
+
+echo "\n-------------- \n";
+var_dump($diff);
 
 
-//var_dump($diff);
-//echo $stringDiff;
+echo "\n-------------- \n";
+// echo $stringDiff;
+
+
+
+echo Diff::toString($diff);
 
 echo StringDiff::zwrocStarySring($diff, $string2) ;
-
-
 //array_splice($arr_alphabet, 2, 0, 'c');
 //unset($array[2]);
 // $array = array_values($array);
