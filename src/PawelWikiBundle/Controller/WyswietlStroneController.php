aa82e204dad5b0e700201bf1d860a57399d4061a
PawelSz <peal1982@gmail.com>
2015-07-05 10:08:40 +0200

    Poczatek controller obsluga szukaj

--- src/PawelWikiBundle/Controller/WyswietlStroneController.php
+++ src/PawelWikiBundle/Controller/WyswietlStroneController.php
@@ -11,6 +11,9 @@ require_once('PobierzRepositoryTrait.php');
 
 use Symfony\Component\HttpFoundation\Request;
 
+
+use Symfony\Component\HttpFoundation\Response;
+
 class WyswietlStroneController extends Controller
 {
 	 use PobierzRepositoryTrait { pobierzRepository as protected; pobierzDoctrine as protected; pobierzMenager as protected; pobierzNazweBaze as protected;}
@@ -137,6 +140,31 @@ class WyswietlStroneController extends Controller
         ->getForm();
         return $form;
     } 
+
+    public function utworzFormSzukajAction($napisyWForm = "Szukaj", Request $request)
+    {
+        $form = $this->createFormBuilder(array(
+        'attr' => array( "class" => "form-search", "role" => "form", ) ))
+            ->add("szukaj", 'text', array( 
+                'attr' => array( 'placeholder' => 'Szukaj', "class" => "span2 search-query" )))
+            ->getForm()  ;
+
+        // $form->handleRequest($request);
+        // if ($form->isValid()) 
+        // {
+        //     var_dump("odczytano szukaj!!!!!!!!!!!!!!!!!!");
+        //     return;
+        // }    
+
+        return $this->wyswietlFormSzukaj( $form ) ;
+    }
+
+
+    private function wyswietlFormSzukaj( $form )
+    {
+        return $this->render( 'PawelWikiBundle:Szukaj:form_szukaj.html.twig', array(
+                'form' => $form->createView() ));
+    }
 
 
     private function przeniescDoStrony( $tytul )
